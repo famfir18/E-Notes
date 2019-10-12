0e14f5b684929adcca61c2dcd4ad3178b7916575
Faisal Amirul Firdaus <faisal.firdaus@fusi24.com>
2019-10-12 20:51:19 +0700

    Second Commit

---
+++ app/src/main/java/com/faisal/e_notes/LihatDataBarang.java
@@ -0,0 +1,13 @@
+package com.faisal.e_notes;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LihatDataBarang extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lihat_data_barang);
+    }
+}
--- app/src/main/java/com/faisal/e_notes/MainActivity.java
+++ app/src/main/java/com/faisal/e_notes/MainActivity.java
@@ -1,5 +1,6 @@
 package com.faisal.e_notes;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.CardView;
@@ -26,5 +27,31 @@ public class MainActivity extends AppCompatActivity {
     void clickInput(View v){
         Animation animScalein = AnimationUtils.loadAnimation(this, R.anim.anim_scale_in);
         v.startAnimation(animScalein);
+        Intent i = new Intent(MainActivity.this, InputDataBarang.class);
+        startActivity(i);
+    }
+
+    @OnClick(R.id.cardLihatData)
+    void clickLihat(View v){
+        Animation animScalein = AnimationUtils.loadAnimation(this, R.anim.anim_scale_in);
+        v.startAnimation(animScalein);
+        Intent i = new Intent(MainActivity.this, LihatDataBarang.class);
+        startActivity(i);
+    }
+
+    @OnClick(R.id.cardAbout)
+    void clickAbout(View v){
+        Animation animScalein = AnimationUtils.loadAnimation(this, R.anim.anim_scale_in);
+        v.startAnimation(animScalein);
+//        Intent i = new Intent(MainActivity.this, LihatDataBarang.class);
+//        startActivity(i);
+    }
+
+    @OnClick(R.id.cardLogout)
+    void clickLogout(View v){
+        Animation animScalein = AnimationUtils.loadAnimation(this, R.anim.anim_scale_in);
+        v.startAnimation(animScalein);
+        Intent i = new Intent(MainActivity.this, LoginActivity.class);
+        startActivity(i);
     }
 }
