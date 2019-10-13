d4e94d175532db947ca1d11293d796ca5c133d6f
Faisal Amirul Firdaus <faisal.firdaus@fusi24.com>
2019-10-13 14:51:12 +0700

    Add About

--- app/src/main/java/com/faisal/e_notes/InputDataBarang.java
+++ app/src/main/java/com/faisal/e_notes/InputDataBarang.java
@@ -1,7 +1,15 @@
 package com.faisal.e_notes;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.Toast;
+
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class InputDataBarang extends AppCompatActivity {
 
@@ -9,5 +17,14 @@ public class InputDataBarang extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_input_data_barang);
+
+        ButterKnife.bind(this);
+    }
+
+    @OnClick(R.id.bt_submit)
+    void clickSubmit(View v){
+        Animation animScalein = AnimationUtils.loadAnimation(this, R.anim.anim_scale_in);
+        v.startAnimation(animScalein);
+        Toast.makeText(this,"Berhasil Input Data", Toast.LENGTH_SHORT).show();
     }
 }
--- app/src/main/java/com/faisal/e_notes/MainActivity.java
+++ app/src/main/java/com/faisal/e_notes/MainActivity.java
@@ -1,12 +1,19 @@
 package com.faisal.e_notes;
 
+import android.app.Dialog;
+import android.content.ActivityNotFoundException;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.CardView;
+import android.util.Log;
 import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
+import android.widget.LinearLayout;
+import android.widget.Toast;
 
 import butterknife.ButterKnife;
 import butterknife.OnClick;
@@ -14,6 +21,7 @@ import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
 
+    Dialog popUpAbout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +29,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         ButterKnife.bind(this);
+        popUpAbout = new Dialog(this);
     }
 
     @OnClick(R.id.cardInput)
@@ -43,8 +52,68 @@ public class MainActivity extends AppCompatActivity {
     void clickAbout(View v){
         Animation animScalein = AnimationUtils.loadAnimation(this, R.anim.anim_scale_in);
         v.startAnimation(animScalein);
-//        Intent i = new Intent(MainActivity.this, LihatDataBarang.class);
-//        startActivity(i);
+        popUpAbout.setContentView(R.layout.pop_up_about);
+        LinearLayout layoutWhatsapp = popUpAbout.findViewById(R.id.layout_whatsapp);
+        LinearLayout layoutInstagram = popUpAbout.findViewById(R.id.layout_instagram);
+        LinearLayout layoutMail = popUpAbout.findViewById(R.id.layout_mail);
+        layoutWhatsapp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String contact = "+6281808917074"; // use country code with your phone number
+                String url = "https://api.whatsapp.com/send?phone=" + contact;
+                try {
+                    PackageManager pm = getApplicationContext().getPackageManager();
+                    pm.getPackageInfo("com.whatsapp", PackageManager.GET_ACTIVITIES);
+                    Intent i = new Intent(Intent.ACTION_VIEW);
+                    i.setData(Uri.parse(url));
+                    startActivity(i);
+                } catch (PackageManager.NameNotFoundException e) {
+                    Toast.makeText(MainActivity.this, "Whatsapp app not installed in your phone", Toast.LENGTH_SHORT).show();
+                    e.printStackTrace();
+                }
+            }
+        });
+        layoutInstagram.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Uri uri = Uri.parse("http://instagram.com/_u/famfir_");
+                Intent likeIng = new Intent(Intent.ACTION_VIEW, uri);
+
+                likeIng.setPackage("com.instagram.android");
+
+                try {
+                    startActivity(likeIng);
+                } catch (ActivityNotFoundException e) {
+                    startActivity(new Intent(Intent.ACTION_VIEW,
+                            Uri.parse("http://instagram.com/famfir_")));
+                }
+            }
+        });
+        layoutMail.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                    String[] TO = {"famfir_@hotmail.com"};
+                    Intent emailIntent = new Intent(Intent.ACTION_SEND);
+                    emailIntent.setData(Uri.parse("mailto:" + TO));
+                    emailIntent.setType("text/plain");
+
+
+                    emailIntent.putExtra(Intent.EXTRA_EMAIL, TO);
+                    emailIntent.putExtra(Intent.EXTRA_SUBJECT, "New Opportunity");
+                    emailIntent.putExtra(Intent.EXTRA_TEXT, "Email message goes here");
+
+                    try {
+                        startActivity(Intent.createChooser(emailIntent, "Send mail..."));
+                        finish();
+                        Toast.makeText(MainActivity.this,"Sending Email...", Toast.LENGTH_SHORT).show();
+                    } catch (android.content.ActivityNotFoundException ex) {
+                        Toast.makeText(MainActivity.this,
+                                "There is no email client installed.", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+        popUpAbout.show();
     }
 
     @OnClick(R.id.cardLogout)
